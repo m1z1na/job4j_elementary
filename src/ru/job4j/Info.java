ed64fd0b20838e8730683c81680bac5e2c97948f
m1z1na <51057800+m1z1na@users.noreply.github.com>
2021-02-01 17:34:21 +0300

    4 Заголовок в файле java [#462373]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-    public class Info {
-        public static void main(String[] args) {
-            System.out.println("01.02.2020");
-        }
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("01.02.2020");
     }
+}
