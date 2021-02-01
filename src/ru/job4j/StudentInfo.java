059587af473fae6c70e493ec1dae1ad897e2743b
m1z1na <51057800+m1z1na@users.noreply.github.com>
2021-02-01 17:22:51 +0300

    3.4.  Заголовок в файле java [#462373]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Elena Mizina");
+        System.out.println("04.08.1995");
+    }
+}
